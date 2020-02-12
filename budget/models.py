c199190e96790745f7248dd7a4b3ad9e2ba49f05
Michał Bartyński <bartynski.michal@gmail.com>
2020-02-12 19:02:48 +0100

    initial commit

---
+++ budget/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# class User(models.Model):
+#     username = models.CharField(max_length=32, unique=True)
+#     password = models.CharField(max_length=32)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Income(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    comment = models.CharField(max_length=255)
+    # user_id = models.ForeignKey(User)
+
+
+class Expenses(models.Model):
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    comment = models.CharField(max_length=255)
