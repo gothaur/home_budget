203ba0fbff2a3196fce844c43a5dd0a032258fe0
Michał Bartyński <bartynski.michal@gmail.com>
2020-02-28 11:32:07 +0100

    nowe funkcjonalnosci

--- budget/context_processor.py
+++ budget/context_processor.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import (
+    User,
+)
 from budget.forms import (
     AddExpenseForm,
     AddIncomeForm,
 )
-from budget.models import Category
+from budget.models import (
+    Category,
+)
 
 
 def sidebars(request):
@@ -12,26 +17,29 @@ def sidebars(request):
         message = "income"
     elif request.path == '/summary/':
         message = "summary"
+    elif request.path == '/users/edit/':
+        message = "settings"
     else:
         message = None
     return {'message': message}
 
 
 def add_entry_form(request):
-    expense_form = AddExpenseForm(
-        # initial={
-        #     'user': request.user.username,
-        #     'category': Category.objects.filter(profile__user__username=request.user.username).order_by('name'),
-        # }
-        # filter_on=request.user.username,
-    )
+    expense_form = AddExpenseForm()
     income_form = AddIncomeForm(
         initial={
             'user': request.user.username,
         }
     )
+    user = User.objects.get(pk=request.user.id)
+    user_categories = user.profile.categories.order_by('name')
+    all_categories = Category.objects.exclude(
+        name__in=[category.name for category in user_categories]
+    )
     context = {
         'expense_form': expense_form,
         'income_form': income_form,
+        'all_categories': all_categories,
+        'user_categories': user_categories,
     }
     return context
