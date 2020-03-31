50735f08ce700bc3c861c3db74c882feb258f711
Michał Bartyński <bartynski.michal@gmail.com>
2020-03-31 10:52:20 +0200

    reset password done

--- auth_ex/urls.py
+++ auth_ex/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
          auth_views.LoginView.as_view(
              template_name='auth_ex/login.html',
              authentication_form=LoginForm,
-             redirect_authenticated_user=False,),
+             redirect_authenticated_user=False, ),
          name='login'
          ),
     path('logout/',
@@ -36,6 +36,36 @@ urlpatterns = [
          ),
          name='logout'),
     path('edit/', SettingsView.as_view(), name='settings'),
+    path(
+        'reset-password/',
+        auth_views.PasswordResetView.as_view(
+            email_template_name='auth_ex/password-reset-email.html',
+            template_name='auth_ex/password-reset-form.html',
+            success_url=reverse_lazy('auth_ex:password-reset-done'),
+        ),
+        name='password-reset'
+    ),
+    path(
+        'reset-password/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='auth_ex/password-reset-done.html',
+        ),
+        name='password-reset-done'
+    ),
+    path(
+        'reset-password/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+            template_name='auth_ex/password-reset-confirm.html',
+            success_url=reverse_lazy('auth_ex:password-reset-complete')
+        ),
+        name='password-reset-confirm',
+    ),
+    path(
+        'reset-password/complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth_ex/password-reset-complete.html'
+        ),
+        name='password-reset-complete',
+    ),
     path('category/delete/<int:category_id>/', SignOutCategory.as_view(), name='signout-category'),
     path('category/add/<int:category_id>/', SignInCategory.as_view(), name='signin-category'),
     path('statute/', StatuteView.as_view(), name='statute'),
